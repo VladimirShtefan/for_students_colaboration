3a73dc6f2ec658a6a0851745d66cd2306b045651
Aidar Putilov <aidarf@mail.ru>
2024-04-28 13:29:21 +0500

    def relesed

--- utils.py
+++ utils.py
@@ -1,8 +1,4 @@
+def multiplication(a, b)
+""" Calculate multiplication of two values """
 
-a = int(input('Enter 1st value: '))
-
-b = int(input('Enter 1st value: '))
-
-m = a * b
-
-print(f'Multiplication:{m}')
+    return a * b
